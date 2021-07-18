357ad34682a98eb8e98ee4a3308d6229329f6cf8
Haïder Alshamma <haideralsh@gmail.com>
2021-07-18 16:09:44 -0400

    Initial commit

---
+++ example/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import FinalActionButton from '../.';
+
+const App = () => {
+  return <FinalActionButton />;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
